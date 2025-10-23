37f00ee4f33f6a5d6ddb50768b3776392d4c9f9c
Alyssa Evans <alyda@me.com>
2025-10-23 10:10:54 -0700

    using rootdir allows nix-shell to be run (and work as intended) from any other directory instead of just the PWD

--- default.nix
+++ default.nix
@@ -7,15 +7,15 @@ let
 in
 
 pkgs.mkShell {
-  buildInputs = cheatShell.buildInputs ++ helixShell.buildInputs;
+  buildInputs = cheatShell.buildInputs ++ helixShell.buildInputs ++ [ pkgs.eza ];
   shellHook = ''
-    export HELIX_CONFIG_DIR=''${PWD}/helix
+    export HELIX_CONFIG_DIR=${rootDir}/helix
     ${helixShell.shellHook or ""}
 
     export EDITOR="hx --config ${rootDir}/helix/helix-config/config.toml"
     alias hx="hx --config $HELIX_CONFIG_DIR/helix-config/config.toml"
 
-    export CHEAT_CONFIG_DIR=''${PWD}/cheat
+    export CHEAT_CONFIG_DIR=${rootDir}/cheat
     ${cheatShell.shellHook or ""}
   '';
 }
