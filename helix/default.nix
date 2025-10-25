06c30ea8656bed8d3c4984cd4069e93deb6b7c4c
Alyssa Evans <alyda@me.com>
2025-10-25 03:18:45 -0700

    deps: helix lsp

--- helix/default.nix
+++ helix/default.nix
@@ -1,7 +1,17 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-  buildInputs = with pkgs; [ helix ];
+  buildInputs = with pkgs; [
+    helix
+
+    # Language servers for languages.toml
+    rust-analyzer                           # Rust LSP
+    nodePackages.typescript-language-server # TypeScript/JavaScript LSP
+    vscode-langservers-extracted            # JSON LSP (vscode-json-languageserver)
+
+    # Debuggers (provides lldb-dap for DAP debugging)
+    lldb
+  ];
 
   shellHook = ''
     export HELIX_CONFIG_DIR=''${HELIX_CONFIG_DIR:-$PWD}
